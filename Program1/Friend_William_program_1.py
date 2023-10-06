a21961b2a3d72332f04f8bdd4b62b77668ee00ea
William R Friend <william.friend777@gmail.com>
2023-10-06 11:09:26 -0400

    Update Friend_William_program_1.py

--- Program1/Friend_William_program_1.py
+++ Program1/Friend_William_program_1.py
@@ -42,12 +42,36 @@ def range_normalize(dataset):
     normalized_dataset = [[dataset[i][0]] + list(scaled_values[i]) + [dataset[i][6]] for i in range(len(dataset))]
     return normalized_dataset  # Return the normalized dataset.
 
-# Function to print a table with specified headers.
+# # Function to print a table with specified headers.
+# def print_table(data_with_metrics, headers):
+#     print(" ".join(headers))  # Print the headers.
+#     # Print each row in the specified format.
+#     for row in data_with_metrics:
+#         print(" ".join(["{:.4f}".format(val) if isinstance(val, float) else val for val in row]))
+
 def print_table(data_with_metrics, headers):
-    print(" ".join(headers))  # Print the headers.
-    # Print each row in the specified format.
+    # Determine maximum widths for each column
+    max_col_widths = [
+        max(len(str(headers[i])),
+            max(len("{:.4f}".format(val) if isinstance(val, float) else str(val))
+                for val in col))
+        for i, col in enumerate(zip(*data_with_metrics))
+    ]
+
+    # Print the headers with alignment
+    header_str = " ".join([headers[i].ljust(max_col_widths[i]) for i in range(len(headers))])
+    print(header_str)
+    print('-' * len(header_str))  # This line prints a separator for clarity
+
+    # Print each row in the specified format
     for row in data_with_metrics:
-        print(" ".join(["{:.4f}".format(val) if isinstance(val, float) else val for val in row]))
+        formatted_row = [
+            "{:.4f}".format(val).rjust(max_col_widths[col_idx]) if isinstance(val, float) 
+            else str(val).ljust(max_col_widths[col_idx]) 
+            for col_idx, val in enumerate(row)
+        ]
+        print(" ".join(formatted_row))
+
 
 # Function for 3-nearest neighbor prediction using Euclidean distance.
 def kNN_3(countries, target_country):
@@ -83,6 +107,24 @@ def weighted_kNN_16(countries, target_country):
     # Return the weighted average CPI.
     return weighted_sum / weight_accumulator if weight_accumulator != 0 else 0
 
+def print_data(data):
+    max_widths_data = [max([len(str(val)) for val in col]) for col in zip(*data)]
+    
+    # Limit max width for columns
+    max_widths_data = [min(width, 11) for width in max_widths_data]
+
+    # The width for the row number is the maximum between "#" and the length of the largest index
+    max_widths = [max(len("#"), len(str(len(data))))] + max_widths_data
+    
+    header = [' '] + [str(i) for i in range(len(data[0]))]
+    header_str = " ".join([header[i].rjust(max_widths[i]) for i in range(len(max_widths))])
+    print(header_str)
+
+    for idx, row in enumerate(data):
+        formatted_row = [str(idx).rjust(max_widths[0])] + [("{:.4f}".format(val) if isinstance(val, float) else str(val)).rjust(max_widths[col_idx + 1]) for col_idx, val in enumerate(row)]
+        print(" ".join(formatted_row))
+
+
 # # Russia data
 russia = ["Russia", 67.62, 31.68, 10.00, 3.87, 12.90]
 
@@ -90,9 +132,20 @@ print("#############################################")
 print("Program output")
 print("#############################################")
 
+print("Original data:")
+#print the original data with numbered rows and columns
+#print_table(enumerate(countries), ["#", "Country", "GDP", "Social", "Life", "Freedom", "Generosity", "CPI"])
+# Change #1, #0.5
+# print_table(countries, ["#", "Country", "GDP", "Social", "Life", "Freedom", "Generosity", "CPI"])
+print_data(countries)
+# print_data(countries, ["#", "0", "1", "2", "3", "4", "5", "6"])
+print('\n')
+
 # 1. What value would a 3-nearest neighbor prediction model using Euclidean distance return for the CPI of Russia?
 cpi_3nn = kNN_3(countries, russia)
 print(f"\nCPI for 3-NN: {cpi_3nn:.4f}\n")
+
+
 
 # 2. What value would a weighted k-NN prediction model return for the CPI of Russia? 
 # Use k=16 (i.e., the full dataset) and a weighting scheme of the reciprocal of the 
@@ -104,6 +157,14 @@ print(f"\nCPI for weighted 16-NN: {cpi_weighted_16nn:.4f}\n")
 # distance return for the CPI of Russia when the descriptive features 
 # have been normalized using range normalization?
 normalized_countries = range_normalize(countries)
+
+# Change #2
+print("Normalized data:")
+# print the normalized data with numbered rows and columns
+# print_data(normalized_countries, ["#", "Country", "GDP", "Social", "Life", "Freedom", "Generosity", "CPI"])
+# print_data(countries, ["#", "0", "1", "2", "3", "4", "5", "6"])
+print_data(normalized_countries)
+
 russia_normalized = ["Russia", 0.6099, 0.3754, 0.0948, 0.5658, 0.9058]
 cpi_3nn_normalized = kNN_3(normalized_countries, russia_normalized)
 print(f"\nAfter normalization: CPI for 3-NN: {cpi_3nn_normalized:.4f}\n")
